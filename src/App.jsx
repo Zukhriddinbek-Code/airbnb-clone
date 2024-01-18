54b454bb17c70afcb20107d7461e7a98ed3eb8d8
Zukhriddinbek <zuhriddinganiyev2000@gmail.com>
2024-01-18 15:58:27 +0900

    done with logo part

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <header>
+      <header className="p-4">
         <a href="" className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +20,7 @@ function App() {
               d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
             />
           </svg>
-          {/* <span className="font-bold text-xl">airbnb</span> */}
+          <span className="font-bold text-xl">airbnb</span>
         </a>
       </header>
     </div>
