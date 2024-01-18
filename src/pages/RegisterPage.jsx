2954327d8a22e2b30866dc4d6d746e6ac48b8188
Zukhriddinbek <zuhriddinganiyev2000@gmail.com>
2024-01-19 01:49:27 +0900

    register page rendreing

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="mt-4 grow flex justify-around items-center">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto">
+          <input type="text" placeholder="your name" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
           <button className="primary">Login</button>
 
           <div className="text-center py-2 text-gray-500">
-            Don't have an account?{" "}
-            <Link className="underline text-black" to={"/register"}>
-              Register now
+            Already a member?{" "}
+            <Link className="underline text-black" to={"/login"}>
+              Login
             </Link>
           </div>
         </form>
@@ -23,4 +24,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
