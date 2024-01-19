9c4548a28a59e05b31d5262275e72fe372f8448e
Zukhriddinbek <zuhriddinganiyev2000@gmail.com>
2024-01-20 01:46:04 +0900

    getting register page input data with useState react hook

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,49 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    axios.get("http://localhost:3000/test");
+  };
+
   return (
     <div className="mt-4 grow flex justify-around items-center">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="your name" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="your name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className="primary">Register</button>
 
           <div className="text-center py-2 text-gray-500">
             Already a member?{" "}
